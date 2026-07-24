a024945cff71d13479a531c070cde489baaa30d8
Ved-nt <vedantsh06@gmail.com>
2026-07-24 18:21:38 +0530

    Week 5 Hands-On 2: Implement data binding

--- Week5/student-course-portal/src/app/pages/home/home.ts
+++ Week5/student-course-portal/src/app/pages/home/home.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.css'
 })
-export class Home {}
+export class Home {
+
+  studentName = "Vedant Sharma";
+
+  course = "Angular Development";
+
+  totalCourses = 8;
+
+  imageUrl = "https://angular.dev/assets/images/press-kit/angular_icon_gradient.gif";
+
+  message = "";
+
+  studentInput = "";
+
+  showMessage() {
+    this.message = "Welcome to Angular! Happy Learning.";
+  }
+
+}
